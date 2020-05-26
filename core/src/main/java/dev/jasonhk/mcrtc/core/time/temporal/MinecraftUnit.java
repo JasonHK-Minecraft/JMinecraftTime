2f7d5ce0f42f7405e39e45a993648a7029ed04e1
Jason Kwok <JasonHK@users.noreply.github.com>
2020-05-26 21:25:16 +0800

    feat(core): added enum `MinecraftUnit`

---
+++ core/src/main/java/dev/jasonhk/mcrtc/core/time/temporal/MinecraftUnit.java
@@ -0,0 +1,80 @@
+package dev.jasonhk.mcrtc.core.time.temporal;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalUnit;
+
+import lombok.NonNull;
+
+public enum MinecraftUnit implements TemporalUnit
+{
+    TICKS("Ticks", UnitType.TIME_BASED, Duration.ofSeconds(3, 600_000_000));
+
+    private final String   name;
+    private final UnitType type;
+    private final Duration duration;
+    private final boolean  durationEstimated;
+
+    MinecraftUnit(String name, UnitType type, Duration duration)
+    {
+        this(name, type, duration, false);
+    }
+
+    MinecraftUnit(String name, UnitType type, Duration duration, boolean durationEstimated)
+    {
+        this.name = name;
+        this.type = type;
+        this.duration = duration;
+        this.durationEstimated = durationEstimated;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <R extends Temporal> R addTo(R temporal, long amount)
+    {
+        return (R) temporal.plus(amount, this);
+    }
+
+    @Override
+    public long between(Temporal temporal1Inclusive, Temporal temporal2Exclusive)
+    {
+        return temporal1Inclusive.until(temporal2Exclusive, this);
+    }
+
+    @Override
+    public boolean isDateBased()
+    {
+        return (this.type == UnitType.DATE_BASED);
+    }
+
+    @Override
+    public boolean isTimeBased()
+    {
+        return (this.type == UnitType.TIME_BASED);
+    }
+
+    @Override
+    public boolean isDurationEstimated()
+    {
+        return this.durationEstimated;
+    }
+
+    @Override
+    public Duration getDuration()
+    {
+        return this.duration;
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.name;
+    }
+
+    private enum UnitType
+    {
+        TIME_BASED,
+        DATE_BASED
+    }
+}
