aef79ef3d47043806671c268ef293bb45032197c
Jason Kwok <JasonHK@users.noreply.github.com>
2020-05-30 17:32:47 +0800

    chore: updated build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent;
+
 group = "dev.jasonhk.mctime"
 version = "0.0.1"
 
 allprojects {
     repositories {
+        mavenCentral()
         maven {
             url = uri("https://plugins.gradle.org/m2/")
         }
@@ -15,11 +18,18 @@ plugins {
 }
 
 dependencies {
-    testImplementation("junit", "junit", "4.12")
+    testImplementation("org.assertj", "assertj-core", "3.16.1")
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.6.2")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+    }
 }
