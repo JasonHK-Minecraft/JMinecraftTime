6511c4cb43a1a3b7865ee44ccbabc37bbb41cd9e
Jason Kwok <JasonHK@users.noreply.github.com>
2020-06-01 15:32:54 +0800

    test: added tests for `MinecraftTime#toNanoOfDay()`

--- src/test/java/dev/jasonhk/minecraft/time/MinecraftTimeTest.java
+++ src/test/java/dev/jasonhk/minecraft/time/MinecraftTimeTest.java
@@ -3,6 +3,8 @@ package dev.jasonhk.minecraft.time;
 import java.time.DateTimeException;
 import java.util.stream.Stream;
 
+import lombok.val;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Nested;
@@ -11,6 +13,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("MinecraftTime")
@@ -22,83 +25,156 @@ public class MinecraftTimeTest
     class Static_Members extends Static_Members_Base
     {
         @Nested
-        class ofNanoOfDay extends ofNanoOfDay_Base
+        class Time_Creator extends Time_Creators_Base
         {
-            @ParameterizedTest(name = "when the nano-of-day value is {0}")
-            @MethodSource
-            void should_return_an_instance_of_MinecraftTime(
-                    final long nanoOfDay,
-                    final long tickOfDay)
+            @Nested
+            class ofNanoOfDay extends ofNanoOfDay_Base
             {
-                MinecraftTimeAssert.assertThat(MinecraftTime.ofNanoOfDay(nanoOfDay))
-                                   .hasTickOfDay(tickOfDay);
+                @ParameterizedTest(name = "when the nano-of-day value is {0}")
+                @MethodSource
+                void should_return_an_instance_of_MinecraftTime(
+                        final long nanoOfDay,
+                        final int tickOfDay)
+                {
+                    MinecraftTimeAssert.assertThat(MinecraftTime.ofNanoOfDay(nanoOfDay))
+                                       .hasTickOfDay(tickOfDay);
+                }
+
+                @ParameterizedTest(name = "when the nano-of-day value is {0}")
+                @MethodSource
+                void should_throw_a_DateTimeException_when_the_range_was_invalid(final long nanoOfDay)
+                {
+                    assertThatThrownBy(() -> { MinecraftTime.ofNanoOfDay(nanoOfDay); })
+                            .isInstanceOf(DateTimeException.class);
+                }
             }
 
-            @ParameterizedTest(name = "when the nano-of-day value is {0}")
-            @MethodSource
-            void should_throw_a_DateTimeException_when_the_range_was_invalid(final long nanoOfDay)
+            @Nested
+            class ofTickOfDay extends ofTickOfDay_Base
             {
-                assertThatThrownBy(() -> { MinecraftTime.ofNanoOfDay(nanoOfDay); })
-                        .isInstanceOf(DateTimeException.class);
+                @ParameterizedTest(name = "when the tick-of-day value is {0}")
+                @MethodSource
+                void should_return_an_instance_of_MinecraftTime(final int tickOfDay)
+                {
+                    MinecraftTimeAssert.assertThat(MinecraftTime.ofTickOfDay(tickOfDay))
+                                       .hasTickOfDay(tickOfDay);
+                }
+
+                @ParameterizedTest(name = "when the tick-of-day value is {0}")
+                @MethodSource
+                void should_throw_a_DateTimeException_when_the_range_was_invalid(final int tickOfDay)
+                {
+                    assertThatThrownBy(() -> { MinecraftTime.ofTickOfDay(tickOfDay); })
+                            .isInstanceOf(DateTimeException.class);
+                }
             }
         }
+    }
 
+    @Nested
+    class Instance_Members extends Instance_Members_Base
+    {
         @Nested
-        class ofTickOfDay extends ofTickOfDay_Base
+        class Time_Accessors extends Time_Accessors_Base
         {
-            @ParameterizedTest(name = "when the tick-of-day value is {0}")
-            @MethodSource
-            void should_return_an_instance_of_MinecraftTime(final long tickOfDay)
+            @Nested
+            class toNanoOfDay extends toNanoOfDay_Base
             {
-                MinecraftTimeAssert.assertThat(MinecraftTime.ofTickOfDay(tickOfDay))
-                                   .hasTickOfDay(tickOfDay);
+                @ParameterizedTest(name = "when the tick-of-day value is {0}")
+                @DisplayName("should return the nano-of-day value from the given tick-of-day value")
+                @MethodSource
+                void should_return_the_nano_of_day_value_from_the_given_tick_of_day_value(
+                        final int tickOfDay,
+                        final long nanoOfDay)
+                {
+                    val time = MinecraftTime.ofTickOfDay(tickOfDay);
+
+                    assertThat(time.toNanoOfDay())
+                            .isBetween(0L, 86_399_999_999_999L)
+                            .isEqualTo(nanoOfDay);
+                }
             }
 
-            @ParameterizedTest(name = "when the tick-of-day value is {0}")
-            @MethodSource
-            void should_throw_a_DateTimeException_when_the_range_was_invalid(final long tickOfDay)
+            @Nested
+            class toTickOfDay extends toTickOfDay_Base
             {
-                assertThatThrownBy(() -> { MinecraftTime.ofTickOfDay(tickOfDay); })
-                        .isInstanceOf(DateTimeException.class);
+                @ParameterizedTest(name = "when the tick-of-day value is {0}")
+                @DisplayName("should return the tick-of-day value")
+                @MethodSource
+                void should_return_the_tick_of_day_value(final int tickOfDay)
+                {
+                    val time = MinecraftTime.ofTickOfDay(tickOfDay);
+
+                    assertThat(time.toTickOfDay())
+                            .isBetween(0, 23999)
+                            .isEqualTo(tickOfDay);
+                }
             }
         }
     }
 
     static class Static_Members_Base
     {
-        static class ofNanoOfDay_Base
+        static class Time_Creators_Base
         {
-            static Stream<Arguments> should_return_an_instance_of_MinecraftTime()
+            static class ofNanoOfDay_Base
             {
-                return Stream.of(
-                        Arguments.of(0L, 18_000L),
-                        Arguments.of(21_599_999_999_999L, 23_999L),
-                        Arguments.of(21_600_000_000_000L, 0L),
-                        Arguments.of(86_399_999_999_999L, 17_999L));
+                static Stream<Arguments> should_return_an_instance_of_MinecraftTime()
+                {
+                    return Stream.of(Arguments.of(0L, 18_000),
+                                     Arguments.of(21_599_999_999_999L, 23_999),
+                                     Arguments.of(21_600_000_000_000L, 0),
+                                     Arguments.of(86_399_999_999_999L, 17_999));
+                }
+
+                static Stream<Arguments> should_throw_a_DateTimeException_when_the_range_was_invalid()
+                {
+                    return Stream.of(
+                            Arguments.of(-1L),
+                            Arguments.of(86_400_000_000_000L));
+                }
             }
 
-            static Stream<Arguments> should_throw_a_DateTimeException_when_the_range_was_invalid()
+            static class ofTickOfDay_Base
             {
-                return Stream.of(
-                        Arguments.of(-1L),
-                        Arguments.of(86_400_000_000_000L));
+                static Stream<Arguments> should_return_an_instance_of_MinecraftTime()
+                {
+                    return Stream.of(Arguments.of(0),
+                                     Arguments.of(23_999));
+                }
+
+                static Stream<Arguments> should_throw_a_DateTimeException_when_the_range_was_invalid()
+                {
+                    return Stream.of(
+                            Arguments.of(-1),
+                            Arguments.of(24_000));
+                }
             }
         }
+    }
 
-        static class ofTickOfDay_Base
+    static class Instance_Members_Base
+    {
+        static class Time_Accessors_Base
         {
-            static Stream<Arguments> should_return_an_instance_of_MinecraftTime()
+            static class toNanoOfDay_Base
             {
-                return Stream.of(
-                        Arguments.of(0L),
-                        Arguments.of(23_999L));
+                static Stream<Arguments> should_return_the_nano_of_day_value_from_the_given_tick_of_day_value()
+                {
+                    return Stream.of(Arguments.of(0, 21_600_000_000_000L),
+                                     Arguments.of(17_999, 86_396_400_000_000L),
+                                     Arguments.of(18_000, 0L),
+                                     Arguments.of(23_999, 21_596_400_000_000L));
+                }
             }
 
-            static Stream<Arguments> should_throw_a_DateTimeException_when_the_range_was_invalid()
+            static class toTickOfDay_Base
             {
-                return Stream.of(
-                        Arguments.of(-1L),
-                        Arguments.of(24_000L));
+                static Stream<Arguments> should_return_the_tick_of_day_value()
+                {
+                    return Stream.of(Arguments.of(0),
+                                     Arguments.of(23_999));
+                }
             }
         }
     }
